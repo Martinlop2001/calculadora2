b550fc09b48d8ea282eedecec64b9910e0445f7d
ivards <ivards34@gmail.com>
2025-05-04 21:06:43 -0300

    agrego codigo de suma

---
+++ suma.py
@@ -0,0 +1,6 @@
+#primer numero es (a) y el segundo numero es (b)
+def suma():
+    a= float(input("ingrese numero que quiere sumar"))
+    b= float(input("ingere el numero por el que lo quiere sumar"))
+    suma = a + b 
+    return suma
